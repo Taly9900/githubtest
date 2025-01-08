5874e85e5bf1269d18e5521ce9e617c4e4a5c8ff
Taly9900 <citlali.mr.design@gmail.com>
2025-01-08 16:19:19 -0600

    Puse un nuevo .java mi primer Hola mundo

--- Lectura.java
+++ Lectura.java
@@ -1,14 +1,9 @@
-package negocio;
-import datos.Impresion;
 import java.util.Scanner;
 
 public class Lectura{
-	Impresion obj = new Impresion();
 	Scanner leer = new Scanner(System.in);
 	public void mensaje(){
 	   System.out.println("Necesito que escribas algo aquí please de nuevo: ");
 	   System.out.println("Hola de nuevo");
-	   String mandar = leer.nextLine();
-	   obj.resultado(mandar);
 	}
 }
