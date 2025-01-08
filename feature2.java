1e6faf7ada8affcac708e128c800b14052bf0726
Taly9900 <citlali.mr.design@gmail.com>
2025-01-08 14:55:20 -0600

    hay un archivo 2 thnks

---
+++ feature2.java
@@ -0,0 +1,8 @@
+public class feature2{
+    public static void main(String[] args) {
+        System.out.println("yo soy en nuevo 2");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("holiiii");
+        }
+    }
+}
